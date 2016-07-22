a03cd6d45a2b9d9a551457079d61c20dea68953f
zengzag <zengzag@hotmail.com>
2016-07-22 20:06:07 +0800

    Markdown Previewer

--- 01_Markdown Previewer/Markdown.js
+++ 01_Markdown Previewer/Markdown.js
@@ -1,7 +1,7 @@
 var Begin = React.createClass({
-   getInitialState:function(){
-       return {textValue:'Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n *[Herman Fassett](http://freecodecamp.com/hermanfassett)*'};
-   },
+    getInitialState:function(){
+        return {textValue:'Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n *[Herman Fassett](http://freecodecamp.com/hermanfassett)*'};
+    },
     textChange:function () {
         this.setState({textValue:this.refs.text.value});
     },
@@ -25,6 +25,6 @@ var Begin = React.createClass({
             </div>
         )
     }
-}); 
+});
 
 ReactDOM.render(<Begin/>,document.getElementById('mark'));
