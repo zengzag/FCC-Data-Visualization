be9432fde724c0ecd6465c5fc16b223c2a7fbf24
zengzag <zengzag@hotmail.com>
2016-08-08 16:45:52 +0800

    react方法，完成

--- 03_RecipeBox/RecipeBox-compiled.js
+++ 03_RecipeBox/RecipeBox-compiled.js
@@ -1,14 +1,37 @@
 'use strict';
 
-var recipes = {};
+console.log(localStorage._zengzag_recipes);
 if (localStorage._zengzag_recipes) {
-    recipes = JSON.parse(localStorage._zengzag_recipes);
+    var recipes = JSON.parse(localStorage._zengzag_recipes);
 } else {
-    recipes = { val: [{ titles: '番茄炒蛋', food: ['番茄', '蛋', '油', '盐'] }, { titles: '番茄炒蛋', food: ['番茄', '蛋', '油', '盐'] }, { titles: '番茄炒蛋', food: ['番茄', '蛋', '油', '盐'] }] };
+    var recipes = {
+        val: [{ title: '番茄炒蛋', food: ['番茄', '蛋', '油', '盐'] }, {
+            title: '麻婆豆腐',
+            food: ['豆腐', '干辣椒', '猪肉', '花椒', '蒜']
+        }, { title: '蜜汁烤翅', food: ['蜂蜜', '糖', '胡椒粉', '耗油'] }]
+    };
 }
 
 var Modal = React.createClass({
     displayName: 'Modal',
+
+    sub: function sub(event) {
+        var num = event.target.getAttribute('data-num');
+        if (num == 'add') {
+            var titles = $('#inputName').val();
+            var foods = $('#inputFood').val().split(',');
+            var temp = { title: titles, food: foods };
+            recipes.val.push(temp);
+        } else {
+            num = parseInt(num);
+            var titles = $('#inputName').val();
+            var foods = $('#inputFood').val().split(',');
+            recipes.val[num].title = titles;
+            recipes.val[num].food = foods;
+        }
+        $('#myModal').modal('hide');
+        update();
+    },
 
     render: function render() {
         return React.createElement(
@@ -56,7 +79,7 @@ var Modal = React.createClass({
                             ),
                             React.createElement(
                                 'button',
-                                { type: 'button', className: 'btn btn-primary' },
+                                { type: 'button', className: 'btn btn-primary', id: 'sub', 'data-num': 'add', onClick: this.sub },
                                 '提交'
                             )
                         )
@@ -73,17 +96,25 @@ var AddRecipe = React.createClass({
     add: function add() {
         $('#myModal').modal('show');
         $('#myModalLabel').text('添加菜谱');
+        $('#inputName').val('');
+        $('#inputFood').val('');
+        $('#sub').attr('data-num', 'add');
     },
 
     render: function render() {
         return React.createElement(
             'div',
-            { id: 'Add' },
+            null,
             React.createElement(
-                'button',
-                { type: 'button', className: 'btn btn-default btn-success', onClick: this.add },
-                '添加'
-            )
+                'div',
+                { id: 'Add' },
+                React.createElement(
+                    'button',
+                    { type: 'button', className: 'btn btn-default btn-success', onClick: this.add },
+                    '添加'
+                )
+            ),
+            React.createElement(Modal, null)
         );
     }
 });
@@ -122,13 +153,12 @@ var ListGroup = React.createClass({
 var PanelList = React.createClass({
     displayName: 'PanelList',
 
+
     render: function render() {
         var Panels = this.props.recipes.map(function (recipe, index) {
-            var href = '#collapse' + index;
-            var id = 'collapse' + index;
             return React.createElement(
                 'div',
-                { className: 'panel panel-default' },
+                { className: 'panel panel-default', key: index },
                 React.createElement(
                     'div',
                     { className: 'panel-heading' },
@@ -138,32 +168,19 @@ var PanelList = React.createClass({
                         React.createElement(
                             'a',
                             { 'data-toggle': 'collapse', 'data-parent': '#accordion',
-                                href: href },
-                            recipe.titles
+                                href: '#collapse' + index },
+                            recipe.title
                         )
                     )
                 ),
                 React.createElement(
                     'div',
-                    { id: id, className: 'panel-collapse collapse' },
+                    { id: 'collapse' + index, className: 'panel-collapse collapse' },
                     React.createElement(
                         'div',
                         { className: 'panel-body' },
                         React.createElement(ListGroup, { list: recipe.food }),
-                        React.createElement(
-                            'div',
-                            { id: 'edit' },
-                            React.createElement(
-                                'button',
-                                { type: 'button', className: 'btn btn-default btn-success', 'data-index': index },
-                                '编辑'
-                            ),
-                            React.createElement(
-                                'button',
-                                { type: 'button', className: 'btn btn-default btn-danger', 'data-index': index },
-                                '删除'
-                            )
-                        )
+                        React.createElement(Edit, { index: index })
                     )
                 )
             );
@@ -177,20 +194,49 @@ var PanelList = React.createClass({
     }
 });
 
-var Main = React.createClass({
-    displayName: 'Main',
+var Edit = React.createClass({
+    displayName: 'Edit',
 
+    edit: function edit(event) {
+        var num = parseInt(event.target.getAttribute('data-index'));
+        $('#myModal').modal('show');
+        var titles = recipes.val[num].title;
+        $('#inputName').val(titles);
+        var foods = recipes.val[num].food.join(',');
+        $('#inputFood').val(foods);
+        $('#myModalLabel').text('修改菜谱');
+        $('#sub').attr('data-num', num);
+    },
+    delete: function _delete(event) {
+        var num = parseInt(event.target.getAttribute('data-index'));
+        recipes.val.splice(num, 1);
+        update();
+    },
 
     render: function render() {
         return React.createElement(
             'div',
-            null,
-            React.createElement(RecipePanels, null),
-            React.createElement(AddRecipe, null),
-            React.createElement(Modal, null)
+            { id: 'edit' },
+            React.createElement(
+                'button',
+                { type: 'button', className: 'btn btn-default btn-success', 'data-index': this.props.index, onClick: this.edit },
+                '编辑'
+            ),
+            React.createElement(
+                'button',
+                { type: 'button', className: 'btn btn-default btn-danger', 'data-index': this.props.index, onClick: this.delete },
+                '删除'
+            )
         );
     }
 });
-ReactDOM.render(React.createElement(Main, null), document.getElementById('main'));
+
+function update() {
+    localStorage._zengzag_recipes = JSON.stringify(recipes);
+    ReactDOM.render(React.createElement(RecipePanels, null), document.getElementById('recipe'));
+}
+
+ReactDOM.render(React.createElement(AddRecipe, null), document.getElementById('buttonAdd'));
+update();
 
 //# sourceMappingURL=RecipeBox-compiled.js.map
--- 03_RecipeBox/RecipeBox.js
+++ 03_RecipeBox/RecipeBox.js
@@ -1,19 +1,39 @@
-var recipes = {};
-if(localStorage._zengzag_recipes)
-{
-    recipes=JSON.parse(localStorage._zengzag_recipes);
+console.log(localStorage._zengzag_recipes);
+if (localStorage._zengzag_recipes) {
+    var recipes = JSON.parse(localStorage._zengzag_recipes);
 }
-else
-{
-    recipes = {val:[{titles:'番茄炒蛋',food:['番茄','蛋','油','盐']},{titles:'番茄炒蛋',food:['番茄','蛋','油','盐']},{titles:'番茄炒蛋',food:['番茄','蛋','油','盐']}]};
+else {
+    var recipes = {
+        val: [{title: '番茄炒蛋', food: ['番茄', '蛋', '油', '盐']}, {
+            title: '麻婆豆腐',
+            food: ['豆腐', '干辣椒', '猪肉', '花椒','蒜']
+        }, {title: '蜜汁烤翅', food: ['蜂蜜', '糖', '胡椒粉', '耗油']}]
+    };
 }
-
 
 var Modal = React.createClass({
+    sub:function (event) {
+        var num = event.target.getAttribute('data-num');
+        if (num == 'add'){
+            var titles = $('#inputName').val();
+            var foods =$('#inputFood').val().split(',');
+            var temp = {title: titles, food:foods};
+            recipes.val.push(temp);
+        }else{
+            num = parseInt(num);
+            var titles = $('#inputName').val();
+            var foods =$('#inputFood').val().split(',');
+            recipes.val[num].title=titles;
+            recipes.val[num].food=foods;
+        }
+        $('#myModal').modal('hide');
+        update();
+    },
+
     render: function () {
         return (
             <div id="modal">
-                <div className="modal fade" id="myModal"  role="dialog"
+                <div className="modal fade" id="myModal" role="dialog"
                      aria-labelledby="myModalLabel" aria-hidden="true">
                     <div className="modal-dialog">
                         <div className="modal-content">
@@ -34,7 +54,7 @@ var Modal = React.createClass({
                                 <button type="button" className="btn btn-default"
                                         data-dismiss="modal">关闭
                                 </button>
-                                <button type="button" className="btn btn-primary">
+                                <button type="button" className="btn btn-primary" id="sub" data-num="add" onClick={this.sub}>
                                     提交
                                 </button>
                             </div>
@@ -47,17 +67,23 @@ var Modal = React.createClass({
 });
 
 var AddRecipe = React.createClass({
-    add:function () {
+    add: function () {
         $('#myModal').modal('show');
         $('#myModalLabel').text('添加菜谱');
+        $('#inputName').val('');
+        $('#inputFood').val('');
+        $('#sub').attr('data-num','add');
     },
 
     render: function () {
         return (
-            <div id="Add">
-                <button type="button" className="btn btn-default btn-success" onClick={this.add}>
-                    添加
-                </button>
+            <div>
+                <div id="Add">
+                    <button type="button" className="btn btn-default btn-success" onClick={this.add}>
+                        添加
+                    </button>
+                </div>
+                <Modal />
             </div>
         );
     }
@@ -67,53 +93,45 @@ var RecipePanels = React.createClass({
     render: function () {
         return (
             <div id="panels">
-                <PanelList recipes={recipes.val} />
+                <PanelList recipes={recipes.val}/>
             </div>
         );
     }
 });
 
 var ListGroup = React.createClass({
-       render: function () {
-           var foodList= this.props.list.map(function(food,index) {
-               return (
-                   <li className="list-group-item" key={index}>{food}</li>
-               );
-           });
+    render: function () {
+        var foodList = this.props.list.map(function (food, index) {
+            return (
+                <li className="list-group-item" key={index}>{food}</li>
+            );
+        });
         return (
             <ul className="list-group">
-        {foodList}
+                {foodList}
             </ul>
         );
     }
 });
 
 var PanelList = React.createClass({
+
     render: function () {
-        var Panels= this.props.recipes.map(function(recipe,index) {
-            var href = '#collapse'+index;
-            var id = 'collapse'+index;
+        var Panels = this.props.recipes.map(function (recipe, index) {
             return (
-                <div className="panel panel-default">
+                <div className="panel panel-default" key={index}>
                     <div className="panel-heading">
                         <h4 className="panel-title">
                             <a data-toggle="collapse" data-parent="#accordion"
-                               href={href}>
-                                {recipe.titles}
+                               href={'#collapse'+index}>
+                                {recipe.title}
                             </a>
                         </h4>
                     </div>
-                    <div id={id} className="panel-collapse collapse">
+                    <div id={'collapse'+index} className="panel-collapse collapse">
                         <div className="panel-body">
-                            <ListGroup  list={recipe.food} />
-                            <div id="edit">
-                                <button type="button" className="btn btn-default btn-success" data-index={index}>
-                                    编辑
-                                </button>
-                                <button type="button" className="btn btn-default btn-danger" data-index={index}>
-                                    删除
-                                </button>
-                            </div>
+                            <ListGroup list={recipe.food}/>
+                            <Edit index={index} />
                         </div>
                     </div>
                 </div>
@@ -128,18 +146,41 @@ var PanelList = React.createClass({
     }
 });
 
-
-
-var Main = React.createClass({
+var Edit = React.createClass({
+    edit:function (event) {
+        var num = parseInt(event.target.getAttribute('data-index'));
+        $('#myModal').modal('show');
+        var titles = recipes.val[num].title;
+        $('#inputName').val(titles);
+        var foods =recipes.val[num].food.join(',');
+        $('#inputFood').val(foods);
+        $('#myModalLabel').text('修改菜谱');
+        $('#sub').attr('data-num',num);
+    },
+    delete:function (event) {
+        var num = parseInt(event.target.getAttribute('data-index'));
+        recipes.val.splice(num,1);
+        update();
+    },
 
     render: function () {
         return (
-            <div>
-                <RecipePanels />
-                <AddRecipe />
-                <Modal />
+            <div id="edit">
+                <button type="button" className="btn btn-default btn-success" data-index={this.props.index} onClick={this.edit}>
+                    编辑
+                </button>
+                <button type="button" className="btn btn-default btn-danger" data-index={this.props.index} onClick={this.delete}>
+                    删除
+                </button>
             </div>
         );
     }
 });
-ReactDOM.render(<Main />, document.getElementById('main'));
+
+function update() {
+    localStorage._zengzag_recipes=JSON.stringify(recipes);
+    ReactDOM.render(<RecipePanels />, document.getElementById('recipe'));
+}
+
+ReactDOM.render(<AddRecipe />, document.getElementById('buttonAdd'));
+update();
